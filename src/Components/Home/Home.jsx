f668f4090f840a4aebe8f6d386e493af08a83ace
MdShuvo18 <mdks33881@gmail.com>
2024-08-28 22:08:11 +0600

    initialized done

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,15 @@
+import Footer from "../Footer/Footer";
+import Navbar from "../Navbar/Navbar";
+
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            This is home
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Home;
