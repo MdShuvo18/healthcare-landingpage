648d14938f19ae31a22003efd360f44ae06ef17d
nahidraz23 <nzamanraz@gmail.com>
2024-08-30 01:25:37 +0600

    button width fixed in choose us section

--- src/Components/ChooseUsSection/ChooseUsSection.jsx
+++ src/Components/ChooseUsSection/ChooseUsSection.jsx
@@ -10,8 +10,8 @@ const ChooseUsSection = () => {
                     <p className='text-secondary-gray'>Lorem ipsum dolor sit amet consectetur adipiscing eli mattis sit phasellus mollis sit aliquam sit nullam.</p>
                 </div>
                 <div className='space-x-6'>
-                    <button className='px-8 py-6 bg-primary-cyan text-white rounded-full font-bold text-lg'>Get started</button>
-                    <button className='px-8 py-6 border-2 rounded-full bg-white text-lg text-primary-cyan'>Talk to sales</button>
+                    <button className='px-9 py-6 bg-primary-cyan text-white rounded-full font-bold text-lg'>Get started</button>
+                    <button className='px-9 py-6 border-2 rounded-full bg-white text-lg text-primary-cyan'>Talk to sales</button>
                 </div>
             </div>
             <div>
